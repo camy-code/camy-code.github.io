b218fbaf2871c4eed7b4f3158faa82388f97c502
camden.warburton <camden.warburton@ucalgary.ca>
2026-07-02 16:38:43 -0600

    Tech skills done

--- src/app/components/technical-skill-card/technical-skill-card.ts
+++ src/app/components/technical-skill-card/technical-skill-card.ts
@@ -9,4 +9,13 @@ import { TechnicalSkills } from '../../interfaces/technicalskills-interface';
 })
 export class TechnicalSkillCard {
   info = input.required<TechnicalSkills>();
+
+  isHovered = false;
+  onHover(state: boolean) {
+    this.isHovered = state;
+  }
+
+  baseCardCSS ="flex flex-row w-[400px] h-[100px] rounded-xl border-1";
+  hoveredCSS = this.baseCardCSS + " bg-gray-100"
+  regularCSS = this.baseCardCSS + " bg-white"
 }
