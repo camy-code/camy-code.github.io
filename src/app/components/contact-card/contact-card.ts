1b450bb6027e621bce299dcb16381f43b2964461
camden.warburton <camden.warburton@ucalgary.ca>
2026-07-02 19:29:49 -0600

    Done for the day, still working on form

--- src/app/components/contact-card/contact-card.ts
+++ src/app/components/contact-card/contact-card.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
-
+import { ContactInterface } from '../../interfaces/contact-interface';
 @Component({
   selector: 'app-contact-card',
   imports: [],
   templateUrl: './contact-card.html',
   styleUrl: './contact-card.css',
 })
-export class ContactCard {}
+export class ContactCard {
+    //   fullName:string,
+    // email:string,
+    // Subject:string,
+    // Other?:string,
+    // message:string
+    subjectOption = [
+      {"text": "Select an option", value:null},
+      {"text":"Freelance Project", value:"project"},
+      {"text":"Tutoring", value:"tutor"},
+      {"text":"Other", value:"other"}
+    ]
+    placeHolderValues = {
+          fullName:"John doe",
+    email:"john@example.com",
+    Subject:this.subjectOption[0],
+    Other:"What is the subject of your message?",
+    message:["Enter your message here",
+      "Tell me about your project idea.",
+      "Tell me about what you would like to learn.",
+      "Elaborate on your the subject here."
+    ]
+    }
+}
