264485b5135e9132f8b64cf0504d575f96b9607c
camden.warburton <camden.warburton@ucalgary.ca>
2026-07-02 18:30:11 -0600

    Working on the contact card

--- src/app/components/home-parts/contact/contact.ts
+++ src/app/components/home-parts/contact/contact.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ContactCard } from '../../contact-card/contact-card';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactCard],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
---
+++ src/app/interfaces/contact-interface.ts
@@ -0,0 +1,7 @@
+export interface ContactInterface {
+    fullName:string,
+    email:string,
+    Subject:string,
+    Other?:string,
+    message:string
+}
