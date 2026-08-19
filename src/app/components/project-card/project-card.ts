97f7b4d878762690ade363be7338fd0886b9999b
camden.warburton <camden.warburton@ucalgary.ca>
2026-08-19 14:09:13 -0600

    Fixed project card

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
 
+import { NgClass } from '@angular/common';
+
 import { ProjectInterface } from '../../interfaces/projects-interface';
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './project-card.html',
   styleUrl: './project-card.css',
 })
