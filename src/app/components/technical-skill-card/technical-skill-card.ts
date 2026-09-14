7fe1f2fe4e4f6174d73a8a4d77fad2bca3725bac
camden.warburton <camden.warburton@ucalgary.ca>
2026-09-13 21:59:59 -0600

    fixed the technical skills card

--- src/app/components/technical-skill-card/technical-skill-card.ts
+++ src/app/components/technical-skill-card/technical-skill-card.ts
@@ -1,9 +1,12 @@
 import { Component, input } from '@angular/core';
 import { TechnicalSkills } from '../../interfaces/technicalskills-interface';
 
+
+import { NgClass } from '@angular/common';
+
 @Component({
   selector: 'app-technical-skill-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './technical-skill-card.html',
   styleUrl: './technical-skill-card.css',
 })
@@ -15,7 +18,7 @@ export class TechnicalSkillCard {
     this.isHovered = state;
   }
 
-  baseCardCSS ="flex flex-row max-w-[385px] h-30 rounded-xl border-1";
-  hoveredCSS = this.baseCardCSS + " bg-gray-100"
-  regularCSS = this.baseCardCSS + " bg-white"
+  // baseCardCSS ="flex flex-row max-w-[385px] h-auto md:h-30 rounded-xl border-1";
+  // hoveredCSS = this.baseCardCSS + " bg-gray-100"
+  // regularCSS = this.baseCardCSS + " bg-white"
 }
